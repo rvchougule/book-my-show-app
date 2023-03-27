b7525defefa078b053206bcd09305452627bbb8f
rohandt0002 <126860059+rohandt0002@users.noreply.github.com>
2023-03-27 06:45:50 +0000

    Day-01 HeroCarousel

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
 import './App.css';
+
+// Routers
 import {Routes, Route} from "react-router-dom";
 
 // React Slick CSS
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 
+// Pages
+import PlayPage from './pages/Play.page';
+import MoviePage from './pages/Movie.page';
+import HomePage from './pages/Home.page';
+
 
 function App() {
   return (
     <Routes>
-      {/* <Route path="/" element={<HomePage/>} />
+      <Route path="/" element={<HomePage/>} />
       <Route path="/movie/:id" element={<MoviePage />} />
-      <Route path="/plays" element={<Plays />} /> */}
+      <Route path="/plays" element={<PlayPage />} />
     </Routes>
   );
 }
---
+++ src/pages/Home.page.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from "react";
+
+// Layout HOC
+import DefaultLayoutHoc from "../layout/Default.layout";
+
+// Components
+import HeroCarousel from "../components/HeroCarousel/HeroCarousel.Component";
+import PosterSlider from "../components/PosterSlider/PosterSlider.Component";
+import EntertainmentCardSlider from "../components/Entertainment/EntertainmentCard.Component";
+
+const HomePage = () => {
+  const [recommendedMovies, setRecommendedMovies] = useState([]);
+  const [premierMovies, setPremierMovies] = useState([]);
+  const [onlineStreamEvents, setOnlineStreamEvents] = useState([]);
+
+  return (
+    <>
+      <HeroCarousel />
+
+      <div className="container mx-auto px-4 md:px-12 my-8">
+        <h1 className="text-2xl font-bold text-gray-800 sm:ml-3 ml-0 my-3">
+          The best of Entertainment
+        </h1>
+        <EntertainmentCardSlider />
+      </div>
+
+      <div className="container mx-auto px-4 md:px-12 my-8">
+        <PosterSlider
+          title="Recommended Movies"
+          subject="List of recommonded movies"
+          posters={recommendedMovies}
+          isDark={false}
+        />
+      </div>
+
+      <div className="bg-premier-800 py-12">
+        <div className="container mx-auto px-4 md:px-12 my-8 flex flex-col gap-3">
+          <div className="hidden md:flex">
+            <img
+              src=""
+              alt="Rupay"
+              className="w-full h-full"
+            />
+          </div>
+          <PosterSlider
+            title="Premiers"
+            subject="Brand new releases every Friday"
+            posters={premierMovies}
+            isDark={true}
+          />
+        </div>
+      </div>
+      <div className="container mx-auto px-4 md:px-12 my-8">
+        <PosterSlider
+          title="Online Streaming Events"
+          subject="Online Stream Events"
+          posters={onlineStreamEvents}
+          isDark={false}
+        />
+      </div>
+    </>
+  );
+};
+
+export default DefaultLayoutHoc(HomePage);
