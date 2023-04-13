9a4b9a41264ec6f63e9243ec654a0484dc94c621
ROHAN CHOUGULE <124122782+rvchougule@users.noreply.github.com>
2023-04-13 12:13:46 +0000

    Pending changes exported from your codespace

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import {Routes,Route} from 'react-router-dom';
+
+// React slick css
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
+function App() {
+  return (
+    <Routes>
+      {/* <Route path="/"  element={<HomePage/>} />
+      <Route path="/movie/:id" element={<MoviePage/>} />
+      <Route path="/plays" element={<Plays/>} /> */}
+    </Routes>
+  );
+}
+
+export default App;
